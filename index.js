35a596b131f7229acba254a0ed806b8a2ab47ba3
Daniel Alves <dalima.dev@gmail.com>
2022-04-20 21:37:56 -0300

    feat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const route = require('./src/routes/medics.route');
 const connectToDatabase = require('./src/database/database');
 
@@ -9,6 +10,7 @@ const app = express();
 connectToDatabase();
 
 app.use(express.json());
+app.use(cors());
 app.use('/medic', route);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
